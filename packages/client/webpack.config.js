58ddd08ac1e06e5b57eb45f95c0709913dc14042
Vitali Vianhura <vengura.code@gmail.com>
2021-07-27 21:18:57 +0300

    Init commit

---
+++ packages/client/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+let config = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: {
+        index: './src/index.ts'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                type: 'javascript/auto',
+                test: /\.(png|xml|json|woff(2?))$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.(s?)css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(m?)js/,
+                resolve: {
+                    fullySpecified: false
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Leela Client'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                }
+            }
+        }
+    }
+};
+
+module.exports = config;
