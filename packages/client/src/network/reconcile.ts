53cf4836376b43b0ba1a729e6d7a03830d598d88
Vitali Vianhura <vengura.code@gmail.com>
2021-08-14 18:35:49 +0300

    Implement smooth error correction

--- packages/client/src/network/reconcile.ts
+++ packages/client/src/network/reconcile.ts
@@ -1,23 +1,28 @@
-import {Tick} from "@leela/common";
 import Ticks from "./Ticks";
 import {State} from "./types";
+import {CLIENT_PREDICT, CLIENT_SMOOTH, CLIENT_SMOOTH_MS} from "../constants/config";
 
 type SequenceId = string;
-type Transaction = unknown;
 
-interface TransactionReducer<S extends State, T extends Transaction> {
-    (accumulator: S, transaction: T, index?: number, transactions?: T[]): S;
+interface Operator<S extends State> {
+    sum(s1: S, s2: S): S;
+    diff(s1: S, s2: S): S;
+    mul(s1: S, scalar: number): S;
+    eq(s1: S, s2: S): boolean;
 }
 
-interface Sequence<S extends State, T extends Transaction> {
-    lastTick: Tick;
-    transactions: T[];
-    reducer: TransactionReducer<S, T>;
+interface Sequence<S extends State> {
+    lastUpdateTick: number;
+    states: S[];
+    error: S;
+    smoothTimer: number;
+    lastAckState: S;
+    operator: Operator<S>
 }
 
 class ReconcileSystem {
 
-    private sequences: Record<SequenceId, Sequence<State, Transaction>>;
+    private sequences: Record<SequenceId, Sequence<State>>;
 
     constructor(
         private readonly ticks: Ticks
@@ -25,36 +30,83 @@ class ReconcileSystem {
         this.reset();
     }
 
-    public register<S extends State, T extends Transaction>(id: SequenceId, reducer: TransactionReducer<S, T>): void {
+    public register<S extends State>(id: SequenceId, operator: Operator<S>): void {
         this.sequences[id] = {
-            lastTick: -1,
-            transactions: [],
-            reducer
+            lastUpdateTick: -1,
+            states: [],
+            error: null,
+            smoothTimer: 0,
+            lastAckState: null,
+            operator
         };
     }
 
-    public add(id: SequenceId, transaction: Transaction): void {
-        const sequence = this.sequences[id];
+    public reconcile<S extends State>(id: SequenceId, state?: S, delta?: number): S {
+        const sequence = this.sequences[id] as Sequence<S>;
 
-        sequence.transactions.push(transaction);
-        sequence.lastTick = this.ticks.client;
-    }
+        if (CLIENT_PREDICT) {
+            const operator = sequence.operator;
 
-    public reconcile<S extends State, T extends Transaction>(id: SequenceId, ackState: S): S {
-        const sequence = this.sequences[id] as Sequence<S, T>;
+            if (sequence.error) {
+                if (CLIENT_SMOOTH) {
+                    const fraction = Math.min(delta, sequence.smoothTimer) / (CLIENT_SMOOTH_MS / 1000);
+                    const correction = operator.mul(sequence.error, fraction);
+                    state = operator.sum(state, correction);
 
-        const {lastTick, transactions, reducer} = sequence;
+                    sequence.smoothTimer = Math.min(sequence.smoothTimer - delta, 0);
+                    if (sequence.smoothTimer == 0) sequence.error = null;
+                } else {
+                    state = operator.sum(state, sequence.error);
 
-        if (transactions.length > 0) {
-            const lag = lastTick - this.ticks.clientAck;
-            const discard = transactions.length - lag;
+                    sequence.error = null;
+                }
+            }
 
-            transactions.splice(0, discard);
+            if (this.isStateUpdate(sequence, state)) {
+                sequence.states.push(state);
+                sequence.lastUpdateTick = this.ticks.client;
+            }
 
-            return transactions.reduce(reducer, ackState);
+            return state;
         }
 
-        return ackState;
+        return sequence.lastAckState || state;
+    }
+
+    private isStateUpdate(sequence: Sequence<State>, state: State) {
+        const states = sequence.states;
+        const operator = sequence.operator;
+
+        if (states.length == 0) {
+            return !(sequence.lastAckState && operator.eq(sequence.lastAckState, state));
+        } else {
+            return !operator.eq(states[states.length - 1], state);
+        }
+    }
+
+    public ack<S extends State>(id: SequenceId, ackState: S): void {
+        const sequence = this.sequences[id] as Sequence<S>;
+
+        if (CLIENT_PREDICT && sequence.states.length > 0) {
+            const lag = sequence.lastUpdateTick - this.ticks.clientAck;
+
+            const states = sequence.states;
+
+            const predIndex = (states.length - 1) - lag;
+            const predState = states[predIndex];
+
+            const operator = sequence.operator;
+
+            sequence.error = operator.diff(predState, ackState);
+
+            if (CLIENT_SMOOTH && sequence.error) {
+                sequence.smoothTimer = CLIENT_SMOOTH_MS / 1000;
+            }
+
+            states.splice(0, predIndex + 1);
+        }
+
+        sequence.lastAckState = ackState;
     }
 
     public reset(): void {
@@ -65,7 +117,6 @@ class ReconcileSystem {
 export {
     SequenceId,
     State,
-    Transaction,
-    TransactionReducer,
+    Operator,
     ReconcileSystem
 };
